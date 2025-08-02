a47ed4e371eb7e3d7929693908ae3ffe3f8a37da
Agentpat <olaideakosile35@gmail.com>
2025-08-02 10:02:40 +0100

    fix: use process.env.PORT for Railway

--- server/Server.js
+++ server/Server.js
@@ -9,6 +9,7 @@ const settingsRoutes = require('./routes/settings');
 const nameRoutes = require('./routes/names');
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
@@ -19,6 +20,6 @@ app.use('/api/names', nameRoutes);
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB connected');
-    app.listen(5000, () => console.log('Server running on port 5000'));
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch(err => console.error(err));
